b18c8660af56f719cfdf11fa039df52ffbbeb376
DovahKriaan <business.tbroben@gmai.com>
2020-03-30 21:25:32 +0200

    new file with updates to config_db

--- config/config_db.php
+++ config/config_db.php
@@ -1,4 +1,13 @@
 <?php 
+$DB_user = "root";
+$DB_pass = "";
+
+try {
+    $DB_connect = new PDO('mysql:host=localhost;dbname=sql-murder-mystery; $DB_user; $DB_pass');
+} catch (PDOException $message) {
+    print "Error connection to DataBase" . $message->getMessage(); <br>
+    die();
+}
 
 
 
